c565c094a83a9d3d638bbbbb0f10fcdcb391fdd7
Blake Lockley <bwlockley@gmail.com>
2021-09-19 08:50:45 +1000

    Update shader to show terrain height

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -1,8 +1,13 @@
 #version 330 core
 
+in vec3 vecPosition;
 out vec4 FragColor;
+
+const vec3 bottomColor = vec3(0.3, 0.2, 0.0);
+const vec3 topColor = vec3(0.0, 1.0, 0.2);
 
 void main()
 {
-    FragColor = vec4(0.0f, 1.0f, 0.2f, 1.0f);
+    vec3 color = mix(bottomColor, topColor, vecPosition.y);
+    FragColor = vec4(color, 1.0f);
 }
--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -4,7 +4,10 @@ layout(location = 0) in vec3 position;
 
 uniform mat4 view, projection;
 
+out vec3 vecPosition;
+
 void main()
 {
     gl_Position = projection * view * vec4(position, 1.0);
+    vecPosition = position;
 }
